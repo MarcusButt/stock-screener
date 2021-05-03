9c42e18fefcb3ce09c02b812413b8957cda1e70a
MarcusButt <MarcusButt@users.noreply.github.com>
2021-05-03 11:56:17 -0230

    Implemented Buy and Sell Points of Strategy with Percent Change

--- gmma-backtest.py
+++ gmma-backtest.py
@@ -32,14 +32,41 @@ for x in emasUsed:
 
 print(df.tail())
 
+position=0
+num=0
+percentChange=[]
+
 for i in df.index:
-    cmin=min(df["Ema_3"][i],df["Ema_5"][i],df["Ema_8"][i],df["Ema_10"][i],df["Ema_12"][i],df["Ema_15"][i],)
+    cmin=min(df["Ema_3"][i],df["Ema_5"][i],df["Ema_8"][i],df["Ema_10"][i],df["Ema_12"][i],df["Ema_15"][i])
     cmax=max(df["Ema_30"][i],df["Ema_35"][i],df["Ema_40"][i],df["Ema_45"][i],df["Ema_50"][i],df["Ema_60"][i])
 
     close=df["Adj Close"][i]
 
     if(cmin>cmax):
         print("Red White Blue")
+        if(position==0):
+            buyPrice=close
+            position=1
+            print("Buy now at "+str(buyPrice))
+
     elif(cmin<cmax):
         print("Blue White Red")
+        if(position==1):
+            position=0
+            sellPrice=close
+            print("Sell now at "+str(sellPrice))
+            pc=(sellPrice/buyPrice-1)*100
+            percentChange.append(pc)
+
+    if(num==df["Adj Close"].count()-1 and position==1):
+        position=0
+        sellPrice=close
+        print("Sell now at "+str(sellPrice))
+        pc=(sellPrice/buyPrice-1)*100
+        percentChange.append(pc)
+
+    num+=1
+
+print(percentChange)
+
 
