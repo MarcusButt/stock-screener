cba2f4526dded6217b8681f522d0181734904862
MarcusButt <MarcusButt@users.noreply.github.com>
2021-05-28 18:46:39 -0230

    Changes from Coaching Session on May 28
    Trying to fix issues with display of sections within the screener

--- index.js
+++ index.js
@@ -2,26 +2,46 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 function toggleDisplay() {
-    var x = document.getElementById("filter-group-body1");
-    if (x.style.display === "none") {
-      x.style.display = "table";
-    } else {
-      x.style.display = "none";
-    }
+  var x = document.getElementById("filter-group-body1");
+  if (x.style.display === "none") {
+    x.style.display = "table";
+  } else {
+    x.style.display = "none";
   }
+}
 
-(function(document){
-  var div = document.getElementById('button-box');
-  var icon = document.getElementById('filter-btn1');
-  var active = false;
+(function toggleRotate(document){
 
-  div.addEventListener('click', function(){
-      if(active){
-      icon.className = 'filter-group-btn';  
-      } else{
-      icon.className = 'filter-btn active';
-      }
+   x = document.getElementsByClassName("button-box");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
 
-      active = !active;
-  });
+      var icon = document.getElementById('filter-btn1');
+      var active = false;
+
+      x[i].addEventListener('click', function(){
+        if(active){
+        icon.className = 'filter-group-btn';  
+        } else{
+        icon.className = 'filter-btn active';
+        }
+  
+        active = !active;
+    });
+   }
+    // document.getElementById(tabName).style.display = "block";
+
+  // var div = document.querySelector('#button-box');
+  // var icon = document.getElementById('filter-btn1');
+  // var active = false;
+
+  // div.addEventListener('click', function(){
+  //     if(active){
+  //     icon.className = 'filter-group-btn';  
+  //     } else{
+  //     icon.className = 'filter-btn active';
+  //     }
+
+  //     active = !active;
+  // });
   })(document);
