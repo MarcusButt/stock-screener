81cf8c11f86962563303cb9f7c0961a6628b856e
MarcusButt <MarcusButt@users.noreply.github.com>
2021-06-02 16:39:21 -0230

    Tried to Use Technique from Filter Solution for Icon Rotations but it Didn't Work

--- index.js
+++ index.js
@@ -8,19 +8,12 @@ function toggleDisplay(id) {
     }
   }
 
-  els = document.getElementsByClassName('button-box')
-  var icon = document.getElementById('filter-btn1');
-  var active = false;
-
-  for(var i=0; i<els.length; ++i){
-    div.addEventListener('click', function(){
-      if(active){
-      icon.className = 'filter-group-btn';  
-      } else {
-      icon.className = 'filter-btn active';
-      }
-
-      active = !active;
-  });
-
+function toggleRotate(id) {
+  var icon = document.getElementById(id);
+  if (icon.style.transform === "rotate(-90deg)") {
+    icon.style.transform = "none";
+  } else {
+    icon.style.transform = "rotate(-90deg)";
   }
+
+}
