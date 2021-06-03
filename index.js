02a49cde69447843c1563d8e38cabbae9a0e603f
MarcusButt <MarcusButt@users.noreply.github.com>
2021-06-03 18:46:59 -0230

    Added New Function for Ticker Search

--- index.js
+++ index.js
@@ -7,12 +7,35 @@ function toggleDisplay(id) {
     }
   }
 
-function toggleRotate(id) {
-  var elm = document.getElementById(id);
-  var className = elm.className;
-  if(className.indexOf('filter-btn-active') === -1) {
-      elm.className = elm.className + 'filter-btn-active';
-  } else {
-      elm.className = elm.className.replace('filter-btn-active', '');
-  }
+// function toggleRotate(id) {
+//   var elm = document.getElementById(id);
+//   var className = elm.className;
+//   if(className.indexOf('filter-btn-active') === -1) {
+//       elm.className = elm.className + 'filter-btn-active';
+//   } else {
+//       elm.className = elm.className.replace('filter-btn-active', '');
+//   }
+// }
+
+
+// (function toggleRotate(id){
+//   var div = document.getElementById('button-box');
+//   var icon = document.getElementById(id);
+//   var open = false;
+  
+//   div.addEventListener('click', function(){
+//     if(open){
+//       icon.className = 'filter-group-btn';  
+//     } else{
+//       icon.className = 'filter-group-btn open';
+//     }
+    
+//     open = !open;
+//   });
+// })(document);
+
+function search_ticker(){
+  var x = document.getElementById("search_ticker_company").value;
+  console.log(x)
 }
+
