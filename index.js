d6e4e99dcf0c50dc304f2f79c2b02de192f2bb72
MarcusButt <MarcusButt@users.noreply.github.com>
2021-06-07 23:32:46 -0230

    Added and Assigned Variables to Filter Parameters for Screener

--- index.js
+++ index.js
@@ -18,7 +18,53 @@ function toggleDisplay(id) {
 // }
 
 function search_ticker(){
-  var x = document.getElementById("search_ticker_company").value;
-  console.log(x)
+  var searchbar_input = document.getElementById("search_ticker_company").value;
+  console.log(searchbar_input)
 }
+
+var market_cap = 0;
+var sector = "All";
+var exchange = "All";
+var employees = 0;
+var volume = 0;
+var avg_volume = 0;
+var dividend = 0;
+var dividend_yield = 0;
+var shares_outstanding = 0;
+var enterprise_value = 0;
+var beta = 0;
+var PE_ratio = 0;
+var PS_ratio = 0;
+var EV_EBITDA = 0;
+var price_timeframe = 0;
+var price_value1 = 0;
+var price_value2 = 0;
+var revenue = 0;
+var gross_income = 0;
+var net_income = 0;
+var EPS_timeframe = 0;
+var EPS_value1 = 0;
+var EPS_value2 = 0;
+var EPS_change_timeframe = 0;
+var EPS_change_value1 = 0;
+var EPS_change_value2 = 0;
+var EBITDA_timeframe = 0;
+var EBITDA_value1 = 0;
+var EBITDA_value2 = 0;
+var EBITDA_change_timeframe = 0;
+var EBITDA_change_value1 = 0;
+var EBITDA_change_value2 = 0;
+var current_ratio = 0;
+var quick_ratio = 0;
+var debt_equity_ratio = 0;
+var lt_debt_equity_ratio = 0;
+var return_on_assets = 0;
+var return_on_equity = 0;
+
+
+
+
+
+
+
 
