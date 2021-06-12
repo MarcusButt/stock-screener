43ea2ad582f2b4a7256d6a204665ecd2ee973162
MarcusButt <MarcusButt@users.noreply.github.com>
2021-06-12 14:13:29 -0230

    Replaced Equal Signs with Colons in Dictionary

--- index.js
+++ index.js
@@ -23,56 +23,56 @@ function search_ticker(){
 }
 
 var screenGeneral = { 
-  market_cap = 0,
-  sector = "All",
-  exchange = "All",
-  employees = 0,
-  volume = 0,
-  avg_volume = 0,
-  dividend = 0,
-  dividend_yield = 0,
-  shares_outstanding = 0,
-  enterprise_value = 0,
-  beta = 0,
+  market_cap : 0,
+  sector : "All",
+  exchange : "All",
+  employees : 0,
+  volume : 0,
+  avg_volume : 0,
+  dividend : 0,
+  dividend_yield : 0,
+  shares_outstanding : 0,
+  enterprise_value : 0,
+  beta : 0,
 }
 
 var screenMultiples = {
-  PE_ratio = 0,
-  PS_ratio = 0,
-  EV_EBITDA = 0,
+  PE_ratio : 0,
+  PS_ratio : 0,
+  EV_EBITDA : 0,
 }
 
 var screenPerformance = {
-  price_timeframe = 0,
-  price_value1 = 0,
-  price_value2 = 0,
+  price_timeframe : 0,
+  price_value1 : 0,
+  price_value2 : 0,
 }
 
 var screenIncome = {
-  revenue = 0,
-  gross_income = 0,
-  net_income = 0,
-  EPS_timeframe = 0,
-  EPS_value1 = 0,
-  EPS_value2 = 0,
-  EPS_change_timeframe = 0,
-  EPS_change_value1 = 0,
-  EPS_change_value2 = 0,
-  EBITDA_timeframe = 0,
-  EBITDA_value1 = 0,
-  EBITDA_value2 = 0,
-  EBITDA_change_timeframe = 0,
-  EBITDA_change_value1 = 0,
-  EBITDA_change_value2 = 0,
+  revenue : 0,
+  gross_income : 0,
+  net_income : 0,
+  EPS_timeframe : 0,
+  EPS_value1 : 0,
+  EPS_value2 : 0,
+  EPS_change_timeframe : 0,
+  EPS_change_value1 : 0,
+  EPS_change_value2 : 0,
+  EBITDA_timeframe : 0,
+  EBITDA_value1 : 0,
+  EBITDA_value2 : 0,
+  EBITDA_change_timeframe : 0,
+  EBITDA_change_value1 : 0,
+  EBITDA_change_value2 : 0,
 }
 
 var screenBalance = {
-  current_ratio = 0,
-  quick_ratio = 0,
-  debt_equity_ratio = 0,
-  lt_debt_equity_ratio = 0,
-  return_on_assets = 0,
-  return_on_equity = 0,
+  current_ratio : 0,
+  quick_ratio : 0,
+  debt_equity_ratio : 0,
+  lt_debt_equity_ratio : 0,
+  return_on_assets : 0,
+  return_on_equity : 0,
 }
 
 var economic_calendar_event = "";
